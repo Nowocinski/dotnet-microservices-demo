327fcedc90de3ecb233c18f01b601915d26454e7
Nowocinski <vito24222@gmail.com>
2022-06-21 12:24:45 +0200

    Configuration connection with MongoDB

---
+++ EShop/EShop.Infrastructure/Mongo/Extension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace EShop.Infrastructure.Mongo
+{
+    public static class Extension
+    {
+        public static void AddMongoDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            var configSection = configuration.GetSection("mongo");
+
+            // var mongoConfig = new MongoConfig();
+            // configSection.Bind(mongoConfig);
+            MongoConfig mongoConfig = configSection.GetSection("mongo") as MongoConfig;
+
+            services.AddSingleton<IMongoClient>(client => {
+                return new MongoClient(mongoConfig.ConnectionString);
+            });
+            services.AddSingleton<IMongoDatabase>(client => {
+                var mongoClient = client.GetService<IMongoClient>();
+                return mongoClient.GetDatabase(mongoConfig.Database);
+            });
+
+            // services.AddSingleton<IDatabaseInitializer, MongoInitializer>();
+        }
+    }
+}
---
+++ EShop/EShop.Infrastructure/Mongo/MongoConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace EShop.Infrastructure.Mongo
+{
+    public class MongoConfig
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
