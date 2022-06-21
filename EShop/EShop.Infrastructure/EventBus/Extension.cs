1bb136a8cf2c90c3f3d66d71908689a4bec35301
Nowocinski <vito24222@gmail.com>
2022-06-21 15:19:23 +0200

    Added RabbitMQ with MassTransit

---
+++ EShop/EShop.Infrastructure/EventBus/Extension.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EShop.Infrastructure.EventBus
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddRabbitMQ(this IServiceCollection services, RabbitMQOption rabbitMQOption)
+        {
+            // establish connection with RabbitMQ
+            services.AddMassTransit(x =>
+            {
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                    cfg.Host(new Uri(rabbitMQOption.ConnectionString), hostcfg =>
+                    {
+                        hostcfg.Username(rabbitMQOption.Username);
+                        hostcfg.Password(rabbitMQOption.Password);
+                    });
+                }));
+            });
+
+            return services;
+        }
+    }
+}
---
+++ EShop/EShop.Infrastructure/EventBus/RabbitMQOption.cs
@@ -0,0 +1,9 @@
+﻿namespace EShop.Infrastructure.EventBus
+{
+    public class RabbitMQOption
+    {
+        public string ConnectionString { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
