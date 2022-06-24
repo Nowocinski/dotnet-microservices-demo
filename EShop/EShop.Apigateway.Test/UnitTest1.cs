c3d222c68f03319a9174083e0752b17f40ab77ab
Nowocinski <vito24222@gmail.com>
2022-06-24 12:38:14 +0200

    Created EShop.Apigateway.Test project

---
+++ EShop/EShop.Apigateway.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EShop.Apigateway.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
