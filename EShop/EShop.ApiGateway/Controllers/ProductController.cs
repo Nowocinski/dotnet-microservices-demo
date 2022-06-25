9cc6d7db69ade95515324091efadd4a32e6385fb
Nowocinski <vito24222@gmail.com>
2022-06-25 09:49:16 +0200

    Created circuit breaker policy

--- EShop/EShop.ApiGateway/Controllers/ProductController.cs
+++ EShop/EShop.ApiGateway/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Polly;
+using Polly.CircuitBreaker;
 using Polly.Fallback;
 using Polly.Retry;
 using Polly.Wrap;
@@ -18,13 +19,19 @@ namespace EShop.ApiGateway.Controllers
     {
         private readonly IBusControl _busControl;
         private readonly IRequestClient<GetProductById> _requestClient;
+        private static readonly int MAX_RETRY_COUNT = 1;
+        #region Policies
+        // Do obsługi wyłączenia serwera, kiedy zaczyna rzucać wyjątkami
+        private static AsyncCircuitBreakerPolicy<IActionResult> _circuitBreaker = Policy<IActionResult>.Handle<Exception>().
+                                                                                    AdvancedCircuitBreakerAsync(0.5, TimeSpan.FromSeconds(30),
+                                                                                                                    2, TimeSpan.FromMinutes(1));
         // Do obsługi zwracania wyjątków
         private readonly AsyncFallbackPolicy<IActionResult> _fallbackPolicy;
         // Do obsługi ponownego wysłania polecenia w przypadku błędu
-        private readonly AsyncRetryPolicy<IActionResult> _retryPolicy;
+        private static AsyncRetryPolicy<IActionResult> _retryPolicy;
         // Do łączenia polityki wyjątków
-        private readonly AsyncPolicyWrap<IActionResult> _wrapPolicy;
-        private readonly int MAX_RETRY_COUNT = 3;
+        private static AsyncPolicyWrap<IActionResult> _wrapPolicy;
+        #endregion
         public ProductController(IBusControl busControl, IRequestClient<GetProductById> requestClient)
         {
             _busControl = busControl;
@@ -32,13 +39,14 @@ namespace EShop.ApiGateway.Controllers
             _fallbackPolicy = Policy<IActionResult>.Handle<Exception>().FallbackAsync(Content("We are experincing issue. Please try after sometime."));
             _retryPolicy = Policy<IActionResult>.Handle<Exception>()
                 ./*RetryAsync*/WaitAndRetryAsync(MAX_RETRY_COUNT, retryCount => TimeSpan.FromSeconds(Math.Pow(3,retryCount)/3));
-            _wrapPolicy = Policy.WrapAsync(_fallbackPolicy, _retryPolicy);
+            _wrapPolicy = Policy.WrapAsync(_circuitBreaker, _retryPolicy);
         }
 
         [HttpGet]
         public async Task<IActionResult> Get(Guid productId)
         {
-            return await _wrapPolicy.ExecuteAsync(async () =>
+            var circuitState = _circuitBreaker.CircuitState;
+            return await /*_wrapPolicy*/_fallbackPolicy.WrapAsync(_wrapPolicy).ExecuteAsync(async () =>
             {
                 var prdct = new GetProductById()
                 {
