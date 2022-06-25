bcd9d6cb0c62b204969032062508f6c9e0da58df
Nowocinski <vito24222@gmail.com>
2022-06-25 11:24:08 +0200

    Configuration redis, dependency injection and masstransit in program.cs

--- EShop/EShop.Cart.Api/Program.cs
+++ EShop/EShop.Cart.Api/Program.cs
@@ -1,3 +1,9 @@
+using EShop.Cart.Api.Handlers;
+using EShop.Cart.DataProvider.Repository;
+using EShop.Cart.DataProvider.Services;
+using EShop.Infrastructure.EventBus;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,43 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Redis
+builder.Services.AddStackExchangeRedisCache((cfg) =>
+{
+    cfg.Configuration = $"{builder.Configuration["Redis:Host"]}:{builder.Configuration["Redis:Port"]}";
+});
+// DI
+builder.Services.AddSingleton<ICartRepository, CartRepository>();
+builder.Services.AddSingleton<ICartService, CartService>();
+// RabbitMQ and MassTransit
+var options = new RabbitMQOption();
+builder.Configuration.GetSection("rabbitmq").Bind(options);
+builder.Services.AddMassTransit(x => {
+    x.AddConsumer<AddCartItemConsumer>();
+    x.AddConsumer<RemoveCartItemConsumer>();
+
+    x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg => {
+        cfg.Host(new Uri(options.ConnectionString), hostConfig => {
+            hostConfig.Username(options.Username);
+            hostConfig.Username(options.Password);
+        });
+
+        cfg.ReceiveEndpoint("add_cart", ep =>
+        {
+            ep.PrefetchCount = 16;
+            ep.UseMessageRetry(retryConfig => { retryConfig.Interval(2, 100); });
+            ep.ConfigureConsumer<AddCartItemConsumer>(provider);
+        });
+
+        cfg.ReceiveEndpoint("remove_cart", ep =>
+        {
+            ep.PrefetchCount = 16;
+            ep.UseMessageRetry(retryConfig => { retryConfig.Interval(2, 100); });
+            ep.ConfigureConsumer<RemoveCartItemConsumer>(provider);
+        });
+    }));
+});
 
 var app = builder.Build();
 
