9cac2bbe571f97de97fa1c7e151514e984b4454c
Nowocinski <vito24222@gmail.com>
2022-06-27 10:38:39 +0200

    Added repository to EShop.Wallet.DataProvider project

---
+++ EShop/EShop.Infrastructure/Wallet/Wallet.cs
@@ -0,0 +1,8 @@
+﻿namespace EShop.Infrastructure.Wallet
+{
+    public class Wallet
+    {
+        public string UserId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ EShop/EShop.Wallet.DataProvider/Repository/IWalletRepository.cs
@@ -0,0 +1,10 @@
+﻿using EShop.Infrastructure.Command.Wallet;
+
+namespace EShop.Wallet.DataProvider.Repository
+{
+    public interface IWalletRepository
+    {
+        Task<bool> AddFunds(AddFunds funds);
+        Task<bool> DeductFunds(DeductFunds funds);
+    }
+}
